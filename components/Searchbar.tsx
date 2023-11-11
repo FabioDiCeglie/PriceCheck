a99525c85318ff3e9a7bd8d06f17cc46c6586b3f
FabioDiCeglie <fabiod@backbase.com>
2023-11-12 00:53:15 +0100

    chore: start implem the SearchBar functionality

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,24 +1,44 @@
 'use client';
 
-const Searchbar = () => {
+import { isValidAmazonProductURL } from '@/utils/helpers';
+import { FormEvent, useState } from 'react';
 
-    const handleSubmit = () => {
-        return '';
+const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    if (!isValidLink) return alert('Please provide a valid Amazon link');
+
+    try {
+      setIsLoading(true);
+
+      // Scrape the product page
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false);
     }
+  };
+
   return (
     <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
-        <input
+      <input
         type='text'
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder='Enter product link'
         className='searchbar-input'
-        />
+      />
 
-        <button type='submit' className='searchbar-btn'>
-            Search
-        </button>
-
+      <button type='submit' className='searchbar-btn' disabled={searchPrompt === ''}>
+        {isLoading ? 'Searching...' : 'Search'}
+      </button>
     </form>
-  )
-}
+  );
+};
 
 export default Searchbar;
