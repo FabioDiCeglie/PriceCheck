0cabce348ec20674f78a72e9a919925bf53b1f08
FabioDiCeglie <fabiod@backbase.com>
2023-11-15 21:43:27 +0100

    chore: return the correct data

--- lib/utils.ts
+++ lib/utils.ts
@@ -33,3 +33,26 @@ export function extractCurrency(element: any) {
   const currencyText = element.text().trim().slice(0, 1);
   return currencyText ? currencyText : "";
 }
+
+// Extracts description from two possible elements from amazon
+export function extractDescription($: any) {
+  // these are possible elements holding description of the product
+  const selectors = [
+    ".a-unordered-list .a-list-item",
+    ".a-expander-content p",
+  ];
+
+  for (const selector of selectors) {
+    const elements = $(selector);
+    if (elements.length > 0) {
+      const textContent = elements
+        .map((_: any, element: any) => $(element).text().trim())
+        .get()
+        .join("\n");
+      return textContent;
+    }
+  }
+
+  // If no matching elements were found, return an empty string
+  return "";
+};
