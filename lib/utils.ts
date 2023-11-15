d848b17940b43e8052116c1530e2038f06215442
FabioDiCeglie <fabiod@backbase.com>
2023-11-15 21:54:59 +0100

    chore: created some helper functions and types

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,14 @@
+import { PriceHistoryItem, Product } from "@/types";
+
+const Notification = {
+  WELCOME: 'WELCOME',
+  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
+  LOWEST_PRICE: 'LOWEST_PRICE',
+  THRESHOLD_MET: 'THRESHOLD_MET',
+}
+
+const THRESHOLD_PERCENTAGE = 40;
+
 export const isValidAmazonProductURL = (url: string): boolean => {
     try {
       const parsedURL = new URL(url);
@@ -18,14 +29,25 @@ export const isValidAmazonProductURL = (url: string): boolean => {
 
 
 
-export const extractPrice = (...elements: any) => {
-    for (const element of elements){
-        const priceText = element.text().trim();
+// Extracts and returns the price from a list of possible elements.
+export function extractPrice(...elements: any) {
+  for (const element of elements) {
+    const priceText = element.text().trim();
 
-        if(priceText) return priceText.replace(/[^\d.]/g, '');
+    if(priceText) {
+      const cleanPrice = priceText.replace(/[^\d.]/g, '');
+
+      let firstPrice; 
+
+      if (cleanPrice) {
+        firstPrice = cleanPrice.match(/\d+\.\d{2}/)?.[0];
+      } 
+
+      return firstPrice || cleanPrice;
     }
+  }
 
-    return '';
+  return '';
 }
 
 // Extracts and returns the currency symbol from an element.
@@ -55,4 +77,54 @@ export function extractDescription($: any) {
 
   // If no matching elements were found, return an empty string
   return "";
+};
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+
+  return averagePrice;
+}
+
+export const getEmailNotifType = (
+  scrapedProduct: Product,
+  currentProduct: Product
+) => {
+  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+
+  if (scrapedProduct.currentPrice < lowestPrice) {
+    return Notification.LOWEST_PRICE as keyof typeof Notification;
+  }
+  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+  }
+  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+    return Notification.THRESHOLD_MET as keyof typeof Notification;
+  }
+
+  return null;
 };
