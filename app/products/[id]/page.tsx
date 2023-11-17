dac7d6fef7894231bff79c489d053edcf3739b72
FabioDiCeglie <fabiod@backbase.com>
2023-11-17 11:01:10 +0100

    chore: added product description

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Modal from '@/components/Modal';
 import PriceInfoCard from '@/components/PriceInfoCard';
 import { getProductById } from '@/lib/actions';
 import { formatNumber } from '@/lib/utils';
@@ -82,7 +83,6 @@ const ProductDetails = async ({ params: { id } }: Props) => {
 
           {/* Product price and reviews */}
           <div className='product-info'>
-
             <div className='flex flex-col gap-2'>
               <p
                 className='text-[34px] text-secondary font-bold
@@ -99,64 +99,104 @@ const ProductDetails = async ({ params: { id } }: Props) => {
             </div>
 
             <div className='flex flex-col gap-4'>
-
               <div className='flex gap-3'>
-
                 <div className='product-stars'>
-                  <Image  src='/assets/icons/star.svg' alt='star' width={16} height={16}/>
+                  <Image
+                    src='/assets/icons/star.svg'
+                    alt='star'
+                    width={16}
+                    height={16}
+                  />
                   <p className='text-sm text-primary-orange font-semibold'>
                     {product.stars || '25'}
                   </p>
                 </div>
 
                 <div className='product-reviews'>
-                  <Image src='/assets/icons/comment.svg' alt='comment' width={16} height={16}/>
+                  <Image
+                    src='/assets/icons/comment.svg'
+                    alt='comment'
+                    width={16}
+                    height={16}
+                  />
                   <p className='text-sm text-secondary font-semibold'>
                     {product.reviewsCount} Reviews
                   </p>
                 </div>
-
               </div>
 
-                <p className='text-sm text-black opacity-50'>
-                  <span className='text-primary-green front-semibold'>93% </span> of buyers have recommended this.
-                </p>
-
+              <p className='text-sm text-black opacity-50'>
+                <span className='text-primary-green front-semibold'>93% </span>{' '}
+                of buyers have recommended this.
+              </p>
             </div>
-
           </div>
 
           {/* Product Information */}
           <div className='my-7 flex flex-col gap-5'>
-
             <div className='flex gap-5 flex-wrap'>
-            <PriceInfoCard 
-                title="Current Price"
-                iconSrc="/assets/icons/price-tag.svg"
-                value={`${product.currency} ${formatNumber(product.currentPrice)}`}
+              <PriceInfoCard
+                title='Current Price'
+                iconSrc='/assets/icons/price-tag.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.currentPrice
+                )}`}
               />
-              <PriceInfoCard 
-                title="Average Price"
-                iconSrc="/assets/icons/chart.svg"
-                value={`${product.currency} ${formatNumber(product.averagePrice)}`}
+              <PriceInfoCard
+                title='Average Price'
+                iconSrc='/assets/icons/chart.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.averagePrice
+                )}`}
               />
-              <PriceInfoCard 
-                title="Highest Price"
-                iconSrc="/assets/icons/arrow-up.svg"
-                value={`${product.currency} ${formatNumber(product.highestPrice)}`}
+              <PriceInfoCard
+                title='Highest Price'
+                iconSrc='/assets/icons/arrow-up.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.highestPrice
+                )}`}
               />
-              <PriceInfoCard 
-                title="Lowest Price"
-                iconSrc="/assets/icons/arrow-down.svg"
-                value={`${product.currency} ${formatNumber(product.lowestPrice)}`}
+              <PriceInfoCard
+                title='Lowest Price'
+                iconSrc='/assets/icons/arrow-down.svg'
+                value={`${product.currency} ${formatNumber(
+                  product.lowestPrice
+                )}`}
               />
-
             </div>
-
           </div>
 
+          <Modal />
         </div>
       </div>
+
+      <div className='flex flex-col gap-16'>
+        {/* Product Description */}
+        <div className='flex flex-col gap-5'>
+          <h3 className='text-2xl text-secondary font-semibold'>
+            Product Description
+          </h3>
+
+          <div className='flex flex-col gap-4'>
+            {product?.description?.split('\n')}
+          </div>
+        </div>
+
+        <button className='btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]'>
+          <Image
+            src='/assets/icons/bag.svg'
+            alt='check'
+            width={22}
+            height={22}
+          />
+
+          <Link href='/' className='text-base text-white'>
+            Buy Now
+          </Link>
+        </button>
+
+      </div>
+
     </div>
   );
 };
